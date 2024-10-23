3cbfa71048d489a64e9d9248040c32f2572cd47b
Khai Tran <khaitran.lewis@gmail.com>
2024-10-23 18:37:10 +0700

    💡 (aws-utils.fish): enhance comments for clarity and maintainability

--- fish/.config/fish/aws-utils.fish
+++ fish/.config/fish/aws-utils.fish
@@ -1,29 +1,39 @@
 #!/usr/bin/env fish
 
+# Function to assume an AWS role using a selected profile from the AWS config.
 function assume_role
+  # Retrieve all AWS profiles from the config file, excluding those with "source".
   set profiles (grep -oP '\[profile \K[^\]]+' ~/.aws/config | grep -v "source")
+  # Use fzf to select a profile interactively.
   set selectedProfile (echo $profiles | string split " " | fzf --header "Select profile" --cycle --ansi --layout=reverse --height=15)
 
+  # Extract the role ARN for the selected profile from the AWS config.
   set role_arn (awk -v profile="$selectedProfile" '
       $0 ~ "\\[profile " profile "\\]" {found=1}
       found && /role_arn/ {print $3; exit}
       /^$/ {found=0}
       ' ~/.aws/config)
 
+  # Extract the MFA serial for the selected profile from the AWS config.
   set mfa_serial (awk -v profile="$selectedProfile" '
       $0 ~ "\\[profile " profile "\\]" {found=1}
       found && /mfa_serial/ {print $3; exit}
       /^$/ {found=0}
       ' ~/.aws/config)
 
+  # Initialize the token code variable.
   set token_code ""
   if test -n "$mfa_serial"
+    # Unlock Bitwarden and retrieve the session ID.
     set -l sessionId (bw unlock | grep -Eo 'BW_SESSION="[^"]+"' | cut -d '"' -f 2 | head -n 1)
+    # Load Bitwarden items using the session ID.
     set -l bwItems (gum spin --title "Loading Bitwarden items" -- bw list items --session "$sessionId")
     set -l selectedBwItem (echo $bwItems | jq -r '.[].name' | fzf --header "Select bitwarden item" --cycle --ansi --layout=reverse --height=15)
+    # Retrieve the TOTP token code for the selected Bitwarden item.
     set token_code (gum spin --title "Loading token code" -- bw get totp $selectedBwItem --session "$sessionId")
   end
   if test -n "$role_arn"
+    # Assume the AWS role using the provided credentials and token code.
     set -l creds (
       gum spin --title "Assuming profile $selectedProfile" -- aws sts assume-role \
         --role-arn "$role_arn" \
@@ -40,6 +50,7 @@ function assume_role
   end
 end
 
+# Function to authenticate an AWS profile and set environment variables.
 function authenticate_aws_profile
   set assume_role_result (assume_role)
   set splitted_result (string split ',,,' $assume_role_result)
@@ -64,6 +75,7 @@ function authenticate_aws_profile
   echo "Authenticated AWS Profile: $selectedProfile. Now you can use AWS CLI commands."
 end
 
+# Function to start an SSM session with a selected EC2 instance.
 function ssm_ec2_instances
   set assume_role_result (assume_role)
   set splitted_result (string split ',,,' $assume_role_result)
@@ -95,8 +107,11 @@ function ssm_ec2_instances
   aws ssm start-session --target $selectedInstance --region $selectedRegion
 end
 
+# Define available tasks for the user to select.
 set tasks 'Authenticate AWS Profile\nSSM to EC2 instances'
 
+# Use fzf to select a task interactively.
+set selectedTask (echo $tasks | string split "\n" | fzf --header "Select task" --cycle --ansi --layout=reverse --height=15)
 set selectedTask (echo $tasks | string split "\n" | fzf --header "Select task" --cycle --ansi --layout=reverse --height=15)
 
 if test "$selectedTask" = "Authenticate AWS Profile"
