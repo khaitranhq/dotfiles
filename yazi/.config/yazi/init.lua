1bb9d30f84f327e8505aba944588d00babf14821
Khai Tran <khaitran.lewis@gmail.com>
2024-10-23 23:15:34 +0700

    🔧 (yazi): update configuration for yazi with new plugins and keymaps
    Add new entries to .gitignore for yazi configuration files to prevent
    them from being tracked. Introduce init.lua to configure bookmarks and
    plugins, enhancing functionality with relative motions and yamb. Add
    keymap.toml to define custom key mappings for bookmark management.
    Include package.toml to specify plugin dependencies. Update yazi.toml
    to configure plugin fetchers, improving the overall setup and
    customization of the yazi environment.

---
+++ yazi/.config/yazi/init.lua
@@ -0,0 +1,38 @@
+require("relative-motions"):setup({ show_numbers = "none", show_motion = true })
+
+-- You can configure your bookmarks by lua language
+local bookmarks = {}
+
+local home_path = ya.target_family() == "windows" and os.getenv("USERPROFILE") or os.getenv("HOME")
+table.insert(bookmarks, {
+	tag = "Downloads",
+	path = home_path .. "/Downloads/",
+	key = "d",
+})
+table.insert(bookmarks, {
+	tag = "VantagePoint",
+	path = home_path .. "/Workspaces/Vantagepoint/",
+	key = "v",
+})
+table.insert(bookmarks, {
+	tag = "Personal",
+	path = home_path .. "/Workspaces/Personal/",
+	key = "p",
+})
+
+require("yamb"):setup({
+	-- Optional, the path ending with path seperator represents folder.
+	bookmarks = bookmarks,
+	-- Optional, recieve notification everytime you jump.
+	jump_notify = true,
+	-- Optional, the cli of fzf.
+	cli = "fzf",
+	-- Optional, a string used for randomly generating keys, where the preceding characters have higher priority.
+	keys = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+	-- Optional, the path of bookmarks
+	path = (ya.target_family() == "windows" and os.getenv("APPDATA") .. "\\yazi\\config\\bookmark")
+		or (os.getenv("HOME") .. "/.config/yazi/bookmark"),
+})
+
+require("git"):setup()
+require("full-border"):setup()
