c47f1e66e962c37a6a2102920d74ed4166d46627
Lewis Tran <khaitran.lewis@gmail.com>
2024-12-16 21:04:39 +0700

    ✨ (config.fish): add 'tmux' to the list of interactive commands for better command tracking ♻️ (tmux.conf): remove 'directory' from the status modules to simplify the status bar display

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -53,7 +53,7 @@ complete -c aws -f -a '(begin; set -lx COMP_SHELL fish; set -lx COMP_LINE (comma
 #
 # It is invoked by the fish shell automatically using its event system.
 function __postexec_notify_on_long_running_commands --on-event fish_postexec
-    set --function interactive_commands 'nvim', 'v'
+    set --function interactive_commands 'nvim', 'v', 'tmux', 't'
     set --function command (string split ' ' $argv[1])
     if contains $command $interactive_commands
         # We quit interactive commands manually,
