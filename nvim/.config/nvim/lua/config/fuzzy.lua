cd5bd4bcaa87d1dda6d23023fe65bd1631a54d39
Khai Tran <khaitran.lewis@gmail.com>
2025-03-13 22:33:52 +0700

    chore(fuzzy.lua): remove animation options for fuzzy finder to simplify configuration and improve performance

--- nvim/.config/nvim/lua/config/fuzzy.lua
+++ nvim/.config/nvim/lua/config/fuzzy.lua
@@ -4,15 +4,6 @@ return {
 		priority = 1000,
 		lazy = false,
 		opts = {
-			animate = {
-				enabled = vim.fn.has("nvim-0.10") == 1,
-				style = "out",
-				easing = "linear",
-				duration = {
-					step = 20, -- ms per step
-					total = 500, -- maximum duration
-				},
-			},
 			indent = {},
 			input = {},
 			lazygit = {},
