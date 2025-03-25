89e12e10623e0f9f0b7ee8c82d3b9114c3642155
Khai Tran <khaitran.lewis@gmail.com>
2025-03-25 17:09:12 +0700

    feat(lsp.lua): add Trouble.nvim plugin for enhanced diagnostics display in Neovim to improve user experience and code navigation

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -13,6 +13,11 @@ return {
 		},
 	},
 	{
+		"folke/trouble.nvim",
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
+		cmd = "Trouble",
+	},
+	{
 		"neovim/nvim-lspconfig",
 		dependencies = {
 			{ "saghen/blink.cmp" },
