f7d935db4a0483e000a5d25c9f58987ef278415b
Khai Tran <khaitran.lewis@gmail.com>
2025-04-02 22:14:18 +0700

    fix(nvim): improve lazy.nvim installation error handling and check for fs_stat compatibility refactor(nvim): streamline the lazy.nvim bootstrap process by using vim.uv for better compatibility

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,22 +1,33 @@
 require("core.general")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
 -- bootstrap lazy.nvim!
-if not vim.loop.fs_stat(lazypath) then
-	vim.api.nvim_echo({ { "  Installing lazy.nvim & plugins ...", "Bold" } }, true, {})
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", repo, lazypath })
-	vim.opt.rtp:prepend(lazypath)
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
-
 vim.opt.rtp:prepend(lazypath)
+
 require("lazy").setup({
 	rocks = {
 		hererocks = true, -- recommended if you do not have global installation of Lua 5.1.
 	},
 	spec = {
 		{ import = "plugins" },
+	},
+	change_detection = {
+		-- automatically check for config file changes and reload the ui
+		enabled = false,
+		notify = false, -- get a notification when changes are found
 	},
 })
 require("core.utils").load_mappings()
