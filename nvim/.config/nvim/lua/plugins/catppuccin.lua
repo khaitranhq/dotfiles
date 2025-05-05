576bba527b7f62f7df513291f4233512fe419f4b
Khai Tran <khaitran.lewis@gmail.com>
2025-05-05 08:46:05 +0700

    feat(catppuccin.lua): add integrations for various plugins to enhance color scheme functionality and user experience

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,15 +1,26 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup(
-                {
-                    flavour = "macchiato"
-                }
-            )
-            vim.cmd.colorscheme "catppuccin"
-        end
-    }
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				flavour = "macchiato",
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					leap = true,
+					lsp_saga = true,
+					markdown = true,
+					mason = true,
+					noice = true,
+					copilot_vim = true,
+					render_markdown = true,
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
