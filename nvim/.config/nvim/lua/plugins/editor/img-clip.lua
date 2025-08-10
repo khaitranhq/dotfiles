f145c0fbc3d0fbaad2d64699760d0b4d1f3c44dd
Khai Tran <khaitran.lewis@gmail.com>
2025-08-10 22:43:02 +0700

    feat(img-clip): add img-clip.nvim plugin configuration for image clipping functionality in Neovim

---
+++ nvim/.config/nvim/lua/plugins/editor/img-clip.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			default = {
+				prompt_for_file_name = false,
+			},
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+	},
+}
