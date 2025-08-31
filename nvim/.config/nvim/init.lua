2143e7f1a8b049c72c958ebe7a97301a74853d3d
Khai Tran <khaitran.lewis@gmail.com>
2025-08-31 21:56:48 +0700

    feat(lsp): add LSP configuration and setup for multiple language servers to enhance development experience chore(nvim): remove deprecated img-clip plugin and integrate it into markdown plugin for better organization feat(cmp): add new completion plugin configuration to improve code completion capabilities

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
 		notify = false, -- get a notification when changes are found
 	},
 })
+require("core.lsp").setup()
 require("core.utils").load_mappings()
 
 -- Setup extra enhancements (nui UI, etc.)
--- nvim/.config/nvim/lua/plugins/language/markdown.lua
+++ nvim/.config/nvim/lua/plugins/language/markdown.lua
@@ -31,4 +31,15 @@ return {
 		end,
 		ft = { "markdown" },
 	},
+	{
+		"HakonHarnes/img-clip.nvim",
+		event = "VeryLazy",
+		opts = {
+			-- default = {
+			-- 	prompt_for_file_name = false,
+			-- },
+			-- add options here
+			-- or leave it empty to use the default settings
+		},
+	},
 }
