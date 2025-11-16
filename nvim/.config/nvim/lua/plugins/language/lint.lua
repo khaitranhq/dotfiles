cd477f76336905d579c08966508a17860263c20a
Khai Tran <khaitran.lewis@gmail.com>
2025-11-16 14:59:08 +0700

    feat(language): add clippy linter for rust
    feat(zellij): change default layout to _custom

--- nvim/.config/nvim/lua/plugins/language/lint.lua
+++ nvim/.config/nvim/lua/plugins/language/lint.lua
@@ -14,11 +14,26 @@ return {
 				typescriptreact = { "eslint" },
 				python = { "mypy" },
 				yaml = { "yamllint" },
+				rust = { "clippy" },
 			}
 
 			require("lint").linters.mypy.args = {
 				"--ignore-missing-imports",
 				"--show-column-numbers",
+			}
+
+			require("lint").linters.clippy.args = {
+				"clippy",
+				"--message-format=json",
+				"--all-targets",
+				"--all-features",
+				"--",
+				"-D",
+				"warnings",
+				"-D",
+				"clippy::all",
+				"-D",
+				"clippy::pedantic",
 			}
 		end,
 	},
