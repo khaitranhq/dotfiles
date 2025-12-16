2d71f9426765e18485553988045bc21bb1a90401
Khai Tran <khaitran.lewis@gmail.com>
2025-12-16 16:49:53 +0700

    chore(git): add credential helper 'store' chore(golangci-lint): add exhaustruct and wsl_v5 linters and varnamelen revive rule feat(lazygit): add os editPreset and promptToReturnFromSubprocess settings chore(nvim): update plugin commits for lazy-lock.json feat(nvim): enhance none-ls.nvim setup with none-ls-extras and formatting autocmd fix(zellij): correct keybind from Alt c to Alt m for ai_commit command

--- nvim/.config/nvim/lua/plugins/language/nonels.lua
+++ nvim/.config/nvim/lua/plugins/language/nonels.lua
@@ -1,21 +1,40 @@
 return {
-	{
-		"nvimtools/none-ls.nvim",
-		config = function()
-			local null_ls = require("null-ls")
-			null_ls.setup({
-				sources = {
-					null_ls.builtins.formatting.stylua,
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.golines,
+          null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.goimports,
           null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.shfmt,
           null_ls.builtins.formatting.yamlfmt,
-					null_ls.builtins.completion.spell,
-					null_ls.builtins.diagnostics.golangci_lint,
-					null_ls.builtins.diagnostics.eslint,
-					null_ls.builtins.diagnostics.yamllint,
-				},
-			})
-		end,
-	},
+          null_ls.builtins.diagnostics.golangci_lint,
+          require("none-ls.diagnostics.eslint"),
+          require("none-ls.diagnostics.yamllint"),
+        },
+        on_attach = function(client, bufnr)
+          if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                vim.lsp.buf.format({ async = false })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
 }
