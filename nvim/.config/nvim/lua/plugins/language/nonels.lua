6bc87d6b4015ba3f6b12ca158cd6beef4cf1c571
khaitranhq <khatran.lewis@gmail.com>
2025-12-27 22:05:33 +0700

    chore(golangci-lint): add ireturn linter and clean revive rules
    feat(nvim): add golines formatter to null-ls sources

--- nvim/.config/nvim/lua/plugins/language/nonels.lua
+++ nvim/.config/nvim/lua/plugins/language/nonels.lua
@@ -11,6 +11,7 @@ return {
         sources = {
           null_ls.builtins.formatting.stylua,
           null_ls.builtins.formatting.gofmt,
+          null_ls.builtins.formatting.golines,
           null_ls.builtins.formatting.gofumpt,
           null_ls.builtins.formatting.goimports,
           null_ls.builtins.formatting.prettier,
