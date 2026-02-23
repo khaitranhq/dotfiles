be0b6e98065a092145eacdfa6c943e70d747589c
Khai Tran <khaitran.lewis@gmail.com>
2026-02-23 22:29:12 +0700

    refactor[editor]: simplify devicon overrides and streamline snacks sources config

--- nvim/.config/nvim/lua/plugins/editor/icons.lua
+++ nvim/.config/nvim/lua/plugins/editor/icons.lua
@@ -1,39 +1,16 @@
 return {
 	{
 		"nvim-tree/nvim-web-devicons",
-		opts = {
-			override = {
-				gitignore = {
-					icon = "",
-					color = "#F14C28",
-					cterm_color = "203",
-					name = "Gitignore",
+		config = function()
+			require("nvim-web-devicons").setup({
+				override = {
+					gitignore = {
+						icon = "",
+						color = "#384d54",
+						name = "Gitignore",
+					},
 				},
-				gitconfig = {
-					icon = "",
-					color = "#F14C28",
-					cterm_color = "203",
-					name = "Gitconfig",
-				},
-				kdl = {
-					icon = "🗒️",
-					color = "#EAD41C",
-					cterm_color = "220",
-					name = "KDL",
-				},
-				["go.mod"] = {
-					icon = "",
-					color = "#519aba",
-					cterm_color = "74",
-					name = "GoMod",
-				},
-				["go.sum"] = {
-					icon = "",
-					color = "#519aba",
-					cterm_color = "74",
-					name = "GoSum",
-				},
-			},
-		},
+			})
+		end,
 	},
 }
