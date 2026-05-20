cb3d5a448c3ea882ed0ad550e7ef33931786cc1c
Khai Tran <khaitran.lewis@gmail.com>
2026-05-20 21:13:48 +0700

    feat(nvim): migrate Go formatting from null-ls to gopls with organize imports on save

--- nvim/.config/nvim/lua/plugins/configs/lsp.lua
+++ nvim/.config/nvim/lua/plugins/configs/lsp.lua
@@ -1,134 +1,158 @@
 local lsp_config = {
-  lua_ls = {
-    settings = {
-      Lua = {
-        diagnostics = {
-          globals = { "vim", "Linemode" },
-        },
-        hint = {
-          enable = true, -- necessary
-        },
-      },
-    },
-  },
-  docker_language_server = {},
-  bashls = {},
-  ts_ls = {},
-  omnisharp = {},
-  oxfmt = {},
-  oxlint = {},
-  golangci_lint_ls = {},
-  just = {},
-  roslyn_ls = {},
-  gopls = {
-    settings = {
-      gopls = {
-        completeUnimported = true,
-        staticcheck = true,
-        analyses = {
-          ST1000 = false,
-          ST1005 = false,
-          QF1003 = true,
-          QF1007 = true,
-          ST1003 = true,
-          unusedparams = true,
-          unusedfuncs = true,
-          unreachable = true,
-          useany = true,
-        },
-        hints = {
-          assignVariableTypes = true,
-          compositeLiteralFields = true,
-          compositeLiteralTypes = true,
-          constantValues = true,
-          functionTypeParameters = true,
-          parameterNames = true,
-          rangeVariableTypes = true,
-        },
-        codelenses = {
-          gc_details = false,
-          generate = false,
-          regenerate_cgo = false,
-          test = false,
-          tidy = false,
-          upgrade_dependency = false,
-          vendor = false,
-        },
-      },
-    },
-  },
+	lua_ls = {
+		settings = {
+			Lua = {
+				diagnostics = {
+					globals = { "vim", "Linemode" },
+				},
+				hint = {
+					enable = true, -- necessary
+				},
+			},
+		},
+	},
+	docker_language_server = {},
+	bashls = {},
+	ts_ls = {},
+	omnisharp = {},
+	oxfmt = {},
+	oxlint = {},
+	golangci_lint_ls = {},
+	just = {},
+	roslyn_ls = {},
+	gopls = {
+		settings = {
+			gopls = {
+				completeUnimported = true,
+				gofumpt = true,
+				staticcheck = true,
+				analyses = {
+					ST1000 = false,
+					ST1005 = false,
+					QF1003 = true,
+					QF1007 = true,
+					ST1003 = true,
+					unusedparams = true,
+					unusedfuncs = true,
+					unreachable = true,
+					useany = true,
+				},
+				hints = {
+					assignVariableTypes = true,
+					compositeLiteralFields = true,
+					compositeLiteralTypes = true,
+					constantValues = true,
+					functionTypeParameters = true,
+					parameterNames = true,
+					rangeVariableTypes = true,
+				},
+				codelenses = {
+					gc_details = false,
+					generate = false,
+					regenerate_cgo = false,
+					test = false,
+					tidy = false,
+					upgrade_dependency = false,
+					vendor = false,
+				},
+			},
+		},
+	},
 }
 
 local blink_config = {
-  keymap = {
-    preset = "default",
-  },
+	keymap = {
+		preset = "default",
+	},
 
-  completion = {
-    documentation = { auto_show = true, auto_show_delay_ms = 500 },
-  },
+	completion = {
+		documentation = { auto_show = true, auto_show_delay_ms = 500 },
+	},
 
-  cmdline = {
-    enabled = true,
-  },
+	cmdline = {
+		enabled = true,
+	},
 
-  appearance = {
-    nerd_font_variant = "normal",
-  },
+	appearance = {
+		nerd_font_variant = "normal",
+	},
 
-  sources = {
-    default = { "lsp", "path", "buffer", "cmdline", "snippets", "omni" },
-    providers = {
-      buffer = {
-        max_items = 4,      -- Limit buffer completion items
-        min_keyword_length = 3, -- Require 3 chars before buffer completion
-      },
-    },
-  },
-  fuzzy = {
-    prebuilt_binaries = {
-      force_version = "v1.10.2",
-    },
-  },
+	sources = {
+		default = { "lsp", "path", "buffer", "cmdline", "snippets", "omni" },
+		providers = {
+			buffer = {
+				max_items = 4, -- Limit buffer completion items
+				min_keyword_length = 3, -- Require 3 chars before buffer completion
+			},
+		},
+	},
+	fuzzy = {
+		prebuilt_binaries = {
+			force_version = "v1.10.2",
+		},
+	},
 }
 
 local M = {}
 
 M.setup = function()
-  -- Configure each LSP server using the new vim.lsp.config API
-  for server, config in pairs(lsp_config) do
-    vim.lsp.config(server, config)
-  end
+	-- Configure each LSP server using the new vim.lsp.config API
+	for server, config in pairs(lsp_config) do
+		vim.lsp.config(server, config)
+	end
 
-  -- Enable all configured LSP servers using the new vim.lsp.enable API
-  local servers = vim.tbl_keys(lsp_config)
-  vim.lsp.enable(servers)
+	-- Enable all configured LSP servers using the new vim.lsp.enable API
+	local servers = vim.tbl_keys(lsp_config)
+	vim.lsp.enable(servers)
 
-  vim.lsp.inlay_hint.enable(true)
+	vim.lsp.inlay_hint.enable(true)
 
-  require("blink.cmp").setup(blink_config)
+	require("blink.cmp").setup(blink_config)
 
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("my.lsp", {}),
-    callback = function(ev)
-      local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		pattern = "*.go",
+		callback = function()
+			local params = vim.lsp.util.make_range_params(0, "utf-8")
+			params.context = { only = { "source.organizeImports" } }
+			-- buf_request_sync defaults to a 1000ms timeout. Depending on your
+			-- machine and codebase, you may want longer. Add an additional
+			-- argument after params if you find that you have to write the file
+			-- twice for changes to be saved.
+			-- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+			local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+			for cid, res in pairs(result or {}) do
+				for _, r in pairs(res.result or {}) do
+					if r.edit then
+						local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+						vim.lsp.util.apply_workspace_edit(r.edit, enc)
+					end
+				end
+			end
+			vim.lsp.buf.format({ async = false })
+		end,
+	})
 
-      -- Auto-format ("lint") on save.
-      -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-      if
-          not client:supports_method("textDocument/willSaveWaitUntil")
-          and client:supports_method("textDocument/formatting")
-      then
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
-          buffer = ev.buf,
-          callback = function()
-            vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
-          end,
-        })
-      end
-    end,
-  })
+	vim.api.nvim_create_autocmd("LspAttach", {
+		group = vim.api.nvim_create_augroup("my.lsp", {}),
+		callback = function(ev)
+			local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+
+			-- Auto-format ("lint") on save.
+			-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
+			if
+				not client:supports_method("textDocument/willSaveWaitUntil")
+				and client:supports_method("textDocument/formatting")
+			then
+				vim.api.nvim_create_autocmd("BufWritePre", {
+					group = vim.api.nvim_create_augroup("my.lsp", { clear = false }),
+					buffer = ev.buf,
+					callback = function()
+						vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 1000 })
+					end,
+				})
+			end
+		end,
+	})
 end
 
 return M
